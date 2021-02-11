621f361d71c6486c2c383acb83fbe5c20fb62e51
Beast <anmoldh121@gmail.com>
2021-02-11 12:42:58 +0530

    relay added

--- go.mod
+++ go.mod
@@ -3,43 +3,9 @@ module github.com/bootstrap
 go 1.15
 
 require (
-	github.com/btcsuite/btcd v0.21.0-beta // indirect
-	github.com/davidlazar/go-crypto v0.0.0-20200604182044-b73af7476f6c // indirect
-	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/google/gopacket v1.1.19 // indirect
-	github.com/google/uuid v1.2.0 // indirect
-	github.com/ipfs/go-ipfs-util v0.0.2 // indirect
-	github.com/ipfs/go-log v1.0.4
-	github.com/koron/go-ssdp v0.0.2 // indirect
-	github.com/libp2p/go-libp2p v0.8.1
-	github.com/libp2p/go-libp2p-autonat-svc v0.1.0
-	github.com/libp2p/go-libp2p-circuit v0.2.1
-	github.com/libp2p/go-libp2p-connmgr v0.2.1
-	github.com/libp2p/go-libp2p-core v0.5.1
-	github.com/libp2p/go-libp2p-discovery v0.3.0
-	github.com/libp2p/go-libp2p-kad-dht v0.5.0
+	github.com/libp2p/go-libp2p v0.13.0 // indirect
+	github.com/libp2p/go-libp2p-kad-dht v0.11.1 // indirect
 	github.com/libp2p/go-libp2p-net v0.1.0 // indirect
 	github.com/libp2p/go-libp2p-noise v0.1.2 // indirect
-	github.com/libp2p/go-libp2p-quic-transport v0.2.3
-	github.com/libp2p/go-libp2p-routing v0.1.0
-	github.com/libp2p/go-libp2p-secio v0.2.2
-	github.com/libp2p/go-libp2p-swarm v0.2.3
-	github.com/libp2p/go-libp2p-tls v0.1.3
-	github.com/multiformats/go-multiaddr v0.3.1
-	github.com/multiformats/go-multiaddr-dns v0.2.0 // indirect
-	github.com/multiformats/go-multiaddr-net v0.2.0
-	github.com/multiformats/go-multistream v0.2.0 // indirect
-	github.com/whyrusleeping/go-logging v0.0.1
-	github.com/whyrusleeping/go-smux-multiplex v3.0.16+incompatible // indirect
-	github.com/whyrusleeping/go-smux-multistream v2.0.2+incompatible // indirect
-	github.com/whyrusleeping/go-smux-yamux v2.0.9+incompatible // indirect
-	github.com/whyrusleeping/yamux v1.2.0 // indirect
-	go.opencensus.io v0.22.5 // indirect
-	go.uber.org/multierr v1.6.0 // indirect
-	go.uber.org/zap v1.16.0 // indirect
-	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
-	golang.org/x/net v0.0.0-20210119194325-5f4716e94777 // indirect
-	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
-	golang.org/x/text v0.3.5 // indirect
-	google.golang.org/genproto v0.0.0-20210126160654-44e461bb6506
+	github.com/libp2p/go-libp2p-quic-transport v0.10.0 // indirect
 )
--- main.go
+++ main.go
@@ -6,16 +6,43 @@ import (
 	mrand "math/rand"
 
 	libp2p "github.com/libp2p/go-libp2p"
+	// circuit "github.com/libp2p/go-libp2p-circuit"
+	"github.com/libp2p/go-libp2p-core/host"
+	net "github.com/libp2p/go-libp2p-core/network"
+
+	// "github.com/libp2p/go-libp2p-core/peerstore"
 	crypto "github.com/libp2p/go-libp2p-crypto"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
-	ma "github.com/multiformats/go-multiaddr"
 	noise "github.com/libp2p/go-libp2p-noise"
-	circuit "github.com/libp2p/go-libp2p-circuit"
+	// quic "github.com/libp2p/go-libp2p-quic-transport"
+	"github.com/libp2p/go-tcp-transport"
+	ma "github.com/multiformats/go-multiaddr"
+	// peer "github.com/libp2p/go-libp2p-core/peer"
 )
+
+var kahdemia *dht.IpfsDHT
+
+type notifier struct {
+	DHT  *dht.IpfsDHT
+	host *host.Host
+}
+
+func (n *notifier) Connected(netw net.Network, conn net.Conn) {
+	// n.host.Peerstore().AddAddr(conn.RemotePeer(), conn.RemoteMultiaddr(), peerstore.PermanentAddrTTL)
+	fmt.Printf("Connection established with %v\n", conn.RemotePeer().Pretty())
+	fmt.Println(n.DHT.RoutingTable().ListPeers())
+	fmt.Println((*n.host).Peerstore().Peers())
+}
+
+func (n *notifier) Listen(netw net.Network, addr ma.Multiaddr)       {}
+func (n *notifier) ListenClose(netw net.Network, addr ma.Multiaddr)  {}
+func (n *notifier) Disconnected(netw net.Network, conn net.Conn)     {}
+func (n *notifier) OpenedStream(netw net.Network, stream net.Stream) {}
+func (n *notifier) ClosedStream(netw net.Network, stream net.Stream) {}
 
 func main() {
 	ctx := context.Background()
-	sourceMultiAddr, _ := ma.NewMultiaddr("/ip4/0.0.0.0/tcp/4000")
+	// sourceMultiAddr := ma.StringCast("/ip4/0.0.0.0/tcp/4000")
 
 	r := mrand.New(mrand.NewSource(int64(10)))
 	prvKey, _, err := crypto.GenerateKeyPairWithReader(crypto.RSA, 2048, r)
@@ -24,10 +51,12 @@ func main() {
 	}
 	host, err := libp2p.New(
 		ctx,
-		libp2p.ListenAddrs(sourceMultiAddr),
+		libp2p.ListenAddrStrings("/ip4/0.0.0.0/tcp/4000", "/ip4/0.0.0.0/udp/4000/quic"),
+		// libp2p.Transport(quic.NewTransport),
+		libp2p.Transport(tcp.NewTCPTransport),
 		libp2p.Security(noise.ID, noise.New),
 		libp2p.Identity(prvKey),
-		libp2p.EnableRelay(circuit.OptHop),
+		// libp2p.EnableRelay(circuit.OptHop),
 	)
 	if err != nil {
 		panic(err)
@@ -35,10 +64,14 @@ func main() {
 
 	fmt.Println("This node: ", host.ID().Pretty(), " ", host.Addrs())
 
-	_, err = dht.New(ctx, host)
+	kahdemia, err = dht.New(ctx, host)
 	if err != nil {
 		panic(err)
 	}
+
+	ntf := notifier{kahdemia, &host}
+
+	host.Network().Notify(&ntf)
 
 	select {}
 }
